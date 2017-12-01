1c59acbf8e6540b40bb3bca3297e6a919f881adb
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-12-01 13:57:45 -0500

    template

--- setup.py
+++ setup.py
@@ -1,31 +1,26 @@
 #!/usr/bin/env python
-req = ['nose','numpy','pandas',]
-pipreq=['simplekml','pymap3d']
-
-import pip
-try:
-    import conda.cli
-    conda.cli.main('install',*req)
-except Exception as e:
-    pip.main(['install'] + req)
-pip.main(['install'] + pipreq)
-
+install_requires = ['numpy','pandas','simplekml',
+        'pymap3d']
+tests_require=['nose','coveralls']
 # %%
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(name='pythonKMLdemo',
-      packages=['kml_demo'],
+      packages=find_packages(),
 	  description='Demo of making KML from Python',
 	  author='Michael Hirsch, Ph.D.',
 	  url='https://github.com/scivision/python-kml-demo',
-      version='0.1',
+      version='0.1.0',
       classifiers=[
       'Intended Audience :: Science/Research',
       'Development Status :: 4 - Beta',
       'License :: OSI Approved :: MIT License',
       'Topic :: Scientific/Engineering :: GIS',
-      'Programming Language :: Python :: 3.5',
-      'Programming Language :: Python :: 3.6',
+      'Programming Language :: Python :: 3',
       ],
+       install_requires=install_requires,
+       python_requires='>=2.7',
+       tests_require=tests_require,
+       extras_require={'tests':tests_require},
 	  )
 
